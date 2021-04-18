c42a74b7447ddf27bcac03af578577c737ae0f28
DocMonster7 <34396385+DocMonster7@users.noreply.github.com>
2021-04-18 18:13:40 +0530

    updated nav bar and added each country in seperate vue

---
+++ src/components/southafrica.vue
@@ -0,0 +1,155 @@
+<template>
+    <div class="container">
+      <!-- {{db_data}} -->
+      <!-- <button type="button" class="btn btn-primary" v-on:click="load_db()">
+        Load
+      </button> -->
+      <br />
+      <br />
+      <div id="all_top_5" v-if="display">
+        <!-- <div id="accordion"> -->
+          <div v-for="country in countires" :key="country.index">
+            
+
+            <div v-if="country == 'south africa'">
+              <!-- <h2>{{ country }}</h2> -->
+
+              <!-- <div class="card">
+                <div class="card-header" id="headingOne">
+                  <h5 class="mb-0">
+                    <button
+                      class="btn btn-link"
+                      data-toggle="collapse"
+                      data-target="#southafrica"
+                      aria-expanded="false"
+                      aria-controls="collapseOne"
+                    >
+                      South Africa
+                    </button>
+                  </h5> -->
+
+                  <span v-show="false"> {{ (i = 0) }} </span>
+
+                  <!-- <div
+                    id="southafrica"
+                    class="collapse show"
+                    aria-labelledby="headingOne"
+                    data-parent="#accordion"
+                  >
+                    <div class="card-body"> -->
+                      <div v-for="category in categories" :key="category.index">
+                        <h3>{{ category }}</h3>
+                        <div v-if="category != 'advertising'">
+                          <div
+                            v-for="date in southafrica_dates"
+                            :key="date.index"
+                          >
+                            <h4>{{ date }}</h4>
+
+                            <table class="table">
+                              <thead>
+                                <th>Tag Name</th>
+                                <th>Frequency</th>
+                              </thead>
+
+                              <tbody v-for="j in limiter" :key="j.index">
+                                <tr>
+                                  <td>
+                                    {{
+                                      countires_list[country][i][category][
+                                        date
+                                      ]["tag_name"][j]
+                                    }}
+                                  </td>
+                                  <td>
+                                    {{
+                                      countires_list[country][i][category][
+                                        date
+                                      ]["frequency"][j]
+                                    }}
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                        <span v-show="false">{{ i++ }}</span>
+                        <!-- <span v-show="false"> {{ i++ }} </span> -->
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <!-- </div>
+          </div>
+        </div>
+      </div> -->
+    <!-- </div> -->
+</template>
+
+<script>
+import db from "../firebaseinit";
+import firebase from "firebase";
+
+export default {
+  
+  data() {
+    return {
+      db_data: {},
+      countires: ["south africa"],
+
+      limiter: [0, 1, 2, 3, 4],
+      upper_limit: 11,
+      lower_limit: 0,
+      display: false,
+      categories: [
+        "building",
+        "leisure",
+        "amenity",
+        "office",
+        "man_made",
+        "advertising",
+        "shop",
+        "craft",
+        "historic",
+        "landuse",
+        "tourism",
+        "boundary",
+      ],
+      countires_list: {
+        
+        "south africa": {},
+      },
+      
+      count: 0,
+      
+      southafrica_dates: [
+        "20170101",
+        "20180101",
+        "20190101",
+        "20200101",
+        "20210101",
+      ],
+    };
+  },
+  created() {
+      this.load_db()
+  },
+  updated() {},
+  methods: {
+    load_db() {
+      this.display = true;
+      this.countires.forEach((country) => {
+        db.ref("/osm_data/analyzed/" + country + "/top_5/data")
+          .once("value", (snap) => {
+            
+            this.countires_list[country] = snap.val();
+            
+          })
+          
+      });
+    },
+    
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,59 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import OSM from '../components/osmdata.vue'
+import India from '../components/india.vue'
+import SouthAfrica from '../components/southafrica.vue'
+import Russia from '../components/russia.vue'
+import Brazil from '../components/brazil.vue'
+import China from '../components/china.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'osm',
+    component: OSM
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/india',
+    name: 'india',
+    component: India
+  },
+  {
+    path: '/southafrica',
+    name: 'southafrica',
+    component: SouthAfrica
+  },
+  {
+    path: '/russia',
+    name: 'russia',
+    component: Russia
+  },
+  {
+    path: '/brazil',
+    name: 'brazil',
+    component: Brazil
+  },
+  {
+    path: '/china',
+    name: 'china',
+    component: China
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
