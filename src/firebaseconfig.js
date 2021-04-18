424bb12d9500338225276620b9a9fa56b082bc77
DocMonster7 <34396385+DocMonster7@users.noreply.github.com>
2021-04-18 14:52:02 +0530

    removed Firebaseconfig file

--- src/firebaseconfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyBTzzXFHncci7RanGMjNfduJ8_471RkYoU",
-    authDomain: "open-street-map-research.firebaseapp.com",
-    databaseURL: "https://open-street-map-research-default-rtdb.firebaseio.com",
-    projectId: "open-street-map-research",
-    storageBucket: "open-street-map-research.appspot.com",
-    messagingSenderId: "609469305650",
-    appId: "1:609469305650:web:aa4196327209d775117d2b"
-};
