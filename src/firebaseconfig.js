c3d2e1bea77cb0fdfb44aa7f1330c8a99118a430
Errol Joshua <34396385+DocMonster7@users.noreply.github.com>
2021-04-19 12:22:42 +0530

    changed secretes to process.env

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,7 +1,7 @@
 
 export default {
-    apiKey: $secrets.FIREBASE_TOKEN,
-    databaseURL: $secrets.FIREBASE_DB_URL,
-    projectId: $secrets.FIREBASE_PROJECT_ID,
+    apiKey: process.env.FIREBASE_TOKEN,
+    databaseURL: process.env.FIREBASE_DB_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
 
 };
