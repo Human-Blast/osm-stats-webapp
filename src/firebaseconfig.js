9ed3855944fe8efd85bf5a57feeff6924df78b6e
DocMonster7 <34396385+DocMonster7@users.noreply.github.com>
2021-04-19 11:35:12 +0530

    added env var to deploy to firebase

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,7 +1,6 @@
-
 export default {
-    apiKey: process.env.FIREBASE_TOKEN,
-    databaseURL: process.env.FIREBASE_DB_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
+    apiKey: process.env.VUE_APP_FIREBASE_TOKEN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
 
 };
